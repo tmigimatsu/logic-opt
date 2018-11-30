f0fe5f9107114c69690e01ccd61895add1444853
Toki Migimatsu <toki.migimatsu@gmail.com>
2018-11-29 22:31:43 -0800

    Add reverse iterator to combination generator

--- include/TrajOpt/planning/combination_generator.h
+++ include/TrajOpt/planning/combination_generator.h
@@ -25,11 +25,17 @@ class CombinationGenerator {
   template<bool Const>
   class Iterator;
 
+  template<typename IteratorT>
+  class ReverseIterator;
+
  public:
 
   using iterator = typename std::conditional_t<std::is_const<ContainerT>::value,
                                                Iterator<true>, Iterator<false>>;
   using const_iterator = Iterator<true>;
+
+  using reverse_iterator = ReverseIterator<iterator>;
+  using const_reverse_iterator = ReverseIterator<const_iterator>;
 
   CombinationGenerator() {}
 
@@ -41,6 +47,9 @@ class CombinationGenerator {
   const_iterator end() const { return const_iterator::end(options_); };
   const_iterator cbegin() const { return const_iterator::begin(options_); };
   const_iterator cend() const { return const_iterator::end(options_); };
+
+  reverse_iterator rbegin() { return ReverseIterator<iterator>(end()); }
+  reverse_iterator rend() { return ReverseIterator<iterator>(begin()); }
 
  private:
 
@@ -58,7 +67,7 @@ class CombinationGenerator<ContainerT>::Iterator {
                                                        typename ContainerT::iterator>;
   using ValueT = typename std::iterator_traits<IteratorT>::value_type;
 
-  using iterator_category = std::forward_iterator_tag;
+  using iterator_category = std::bidirectional_iterator_tag;
   using value_type = std::vector<ValueT>;
   using difference_type = ptrdiff_t;
   using pointer = typename std::conditional_t<Const, const value_type*, value_type*>;
@@ -69,7 +78,7 @@ class CombinationGenerator<ContainerT>::Iterator {
   Iterator& operator++();
   Iterator& operator--();
   bool operator==(const Iterator& other) const;
-  bool operator!=(const Iterator& other) const { return !(*this == other); };
+  bool operator!=(const Iterator& other) const { return !(*this == other); }
   reference operator*();
 
  private:
@@ -86,34 +95,37 @@ class CombinationGenerator<ContainerT>::Iterator {
 };
 
 template<typename ContainerT>
-template<bool Const>
-typename CombinationGenerator<ContainerT>::template Iterator<Const>
-CombinationGenerator<ContainerT>::Iterator<Const>::begin(const std::vector<ContainerT*>& options) {
-  std::vector<IteratorT> it_options;
-  it_options.reserve(options.size());
-  for (ContainerT* option : options) {
-    it_options.push_back(option->begin());
-  }
-  return Iterator(options, std::move(it_options));
-}
+template<typename IteratorT>
+class CombinationGenerator<ContainerT>::ReverseIterator {
 
-template<typename ContainerT>
-template<bool Const>
-typename CombinationGenerator<ContainerT>::template Iterator<Const>
-CombinationGenerator<ContainerT>::Iterator<Const>::end(const std::vector<ContainerT*>& options) {
-  std::vector<IteratorT> it_options;
-  it_options.reserve(options.size());
-  for (ContainerT* option : options) {
-    it_options.push_back(option->end());
-  }
-  return Iterator(options, std::move(it_options));
-}
+ public:
+
+  using iterator_category = typename std::iterator_traits<IteratorT>::iterator_category;
+  using value_type = typename std::iterator_traits<IteratorT>::value_type;
+  using difference_type = typename std::iterator_traits<IteratorT>::difference_type;
+  using pointer = typename std::iterator_traits<IteratorT>::pointer;
+  using reference = typename std::iterator_traits<IteratorT>::reference;
+
+  ReverseIterator(IteratorT&& it);
+
+  ReverseIterator& operator++();
+  ReverseIterator& operator--();
+  bool operator==(const ReverseIterator& other) const { return it_ == other.it_; }
+  bool operator!=(const ReverseIterator& other) const { return !(*this == other); }
+  reference operator*() { return *it_; }
+
+ private:
+
+  IteratorT it_;
+
+};
 
 template<typename ContainerT>
 template<bool Const>
 CombinationGenerator<ContainerT>::Iterator<Const>::Iterator(const std::vector<ContainerT*>& options,
                                                             std::vector<IteratorT>&& it_options)
     : options_(options), it_options_(std::move(it_options)), combination_(options_.size()) {
+  if (it_options_.front() == options_.front()->end()) return;
   for (size_t i = 0; i < options_.size(); i++) {
     combination_[i] = *it_options_[i];
   }
@@ -226,6 +238,65 @@ CombinationGenerator<ContainerT>::Iterator<Const>::operator*() {
     throw std::out_of_range("ParameterGenerator::iterator::operator*(): Cannot dereference.");
   }
   return combination_;
+}
+
+template<typename ContainerT>
+template<bool Const>
+typename CombinationGenerator<ContainerT>::template Iterator<Const>
+CombinationGenerator<ContainerT>::Iterator<Const>::begin(const std::vector<ContainerT*>& options) {
+  std::vector<IteratorT> it_options;
+  it_options.reserve(options.size());
+  for (ContainerT* option : options) {
+    it_options.push_back(option->begin());
+  }
+  return Iterator(options, std::move(it_options));
+}
+
+template<typename ContainerT>
+template<bool Const>
+typename CombinationGenerator<ContainerT>::template Iterator<Const>
+CombinationGenerator<ContainerT>::Iterator<Const>::end(const std::vector<ContainerT*>& options) {
+  std::vector<IteratorT> it_options;
+  it_options.reserve(options.size());
+  for (ContainerT* option : options) {
+    it_options.push_back(option->end());
+  }
+  return Iterator(options, std::move(it_options));
+}
+
+template<typename ContainerT>
+template<typename IteratorT>
+CombinationGenerator<ContainerT>::ReverseIterator<IteratorT>::ReverseIterator(IteratorT&& it)
+    : it_(std::move(it)) {
+  if (it_ == IteratorT::begin(it_.options_)) {
+    it_ = IteratorT::end(it_.options_);
+  } else {
+    --it_;
+  }
+}
+
+template<typename ContainerT>
+template<typename IteratorT>
+typename CombinationGenerator<ContainerT>::template ReverseIterator<IteratorT>&
+CombinationGenerator<ContainerT>::ReverseIterator<IteratorT>::operator++() {
+  if (it_ == IteratorT::begin(it_.options_)) {
+    it_ = IteratorT::end(it_.options_);
+  } else if (it_ != IteratorT::end(it_.options_)) {
+    --it_;
+  }
+  return *this;
+}
+
+template<typename ContainerT>
+template<typename IteratorT>
+typename CombinationGenerator<ContainerT>::template ReverseIterator<IteratorT>&
+CombinationGenerator<ContainerT>::ReverseIterator<IteratorT>::operator--() {
+  if (it_ == IteratorT::end(it_.options_)) {
+    it_ = IteratorT::begin(it_.options_);
+  } else {
+    ++it_;
+  }
+  return *this;
 }
 
 }  // namespace TrajOpt
